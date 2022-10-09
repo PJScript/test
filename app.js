267680b66005f2a42576468f0bbd712154660977
Gennect_3 <gennect_3@Gennect-3ui-MacBookAir.local>
2022-10-09 21:29:59 +0900

    fix path

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const path = require('path')
 const app = express()
 
 
-app.use(express.static('public'))
-
+// app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get('/', (req,res) => {
   res.redirect("/group")
