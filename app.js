c2adc303b2822a8c2f3833ab75e34aa90dc07d01
Gennect_3 <gennect_3@Gennect-3ui-MacBookAir.local>
2022-10-10 15:06:49 +0900

    cached

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const cors = require('cors')
 
+const mysql = require("mysql");
 
+const con = mysql.createConnection({
+	host: process.env.HOST,
+	port: process.env.PORT,
+	user: process.env.ACCOUNT,
+	password: process.env.PASSWORD,
+	database: process.env.DATABASE_NAME
+});
+
+con.connect(err => {
+	if (err) console.log("MySQL 연결 실패 : ", err);
+	console.log("MySQL Connected!!!");
+})
+출처: https://jy-tblog.tistory.com/51 [jy.log:티스토리]
 // app.use(express.static('public'))
+app.use(cors({
+  origin:"*"
+}))
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.json()); 
+app.use(express.urlencoded( {extended : false } ));
 
 app.get('/', (req,res) => {
-  res.redirect("/group")
+  // res.redirect("/group")
+  res.sendFile(__dirname + '/public/home.html');
+
 })
 
-app.get('/group', (req,res) => {
-  res.sendFile(__dirname + '/public/home.html');
-})
+// app.get('/group', (req,res) => {
+//   res.sendFile(__dirname + '/public/home.html');
+// })
 
 app.get('/make' , (req,res) => {
   res.sendFile(__dirname + '/public/make.html')
@@ -48,6 +70,37 @@ app.get('/result', (req,res) => {
 app.get('/nicknamedone' , (req,res) => {
   res.sendFile(__dirname + "/public/addnicknamedone.html")
 })
+
+
+
+
+app.post(`/join`, (req,res) => {
+  console.log(req.body)
+  res.json("join")
+})
+
+
+app.post('/makegroup', (req,res) => {
+  console.log(req.body,"makegroup")
+  res.json("make group")
+})
+
+app.post('/addnickname', (req,res) => {
+  res.json("add nickname")
+})
+
+app.post('/vote', (req,res) => {
+  
+})
+
+
+app.post('/login', (req,res) => {
+  console.log(req.body,"login")
+  res.json("login")
+})
+
+app.post('/')
+
 app.listen(8080,() => {
   console.log("on")
 })
